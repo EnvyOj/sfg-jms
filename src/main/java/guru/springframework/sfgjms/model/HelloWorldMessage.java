a1b72bd248a4a9099e77aa48f3f01dc8c71c4492
Hunady Dominik <dominik.hunady@isdd.sk>
2021-04-27 09:03:46 +0200

    added hello world configuration

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serializeVersionUUID = 3264277122213098L;
+
+    private UUID id;
+    private String message;
+}
